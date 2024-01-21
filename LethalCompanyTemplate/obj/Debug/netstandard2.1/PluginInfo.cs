27d5277541cf0817a9d45d40d6411eadaf3f1712
swAAn01 <jaeswan@outlook.com>
2024-01-21 16:39:09 -0600

    refactor directory

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace KnucklesMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "KnucklesMod";
-        public const string PLUGIN_NAME = "KnucklesMod";
-        public const string PLUGIN_VERSION = "1.4.2";
-    }
-}
